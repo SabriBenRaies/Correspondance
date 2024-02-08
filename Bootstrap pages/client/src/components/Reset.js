a40ce2a4e17bcea217654a34614269d450f6529f
Sabri Ben Raies <sabri.benrais@gmail.com>
2024-02-08 03:47:13 +0100

    Bootstrap page Reset

--- Bootstrap pages/client/src/components/Reset.js
+++ Bootstrap pages/client/src/components/Reset.js
@@ -1,74 +1,102 @@
-import React, { useEffect } from 'react'
-import toast, {Toaster} from 'react-hot-toast'
-import {useFormik} from 'formik'
-import { resetPasswordValidate } from '../helper/validate'
-import { resetPassword } from '../helper/helper'
-import { useAuthStore } from '../store/store'
-import {useNavigate, Navigate} from 'react-router-dom'
-import useFetch from '../hooks/fetch.hook'
+import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { useFormik } from 'formik';
+import { resetPasswordValidate } from '../helper/validate';
+import { resetPassword } from '../helper/helper';
+import { useAuthStore } from '../store/store';
+import { useNavigate, Navigate } from 'react-router-dom';
+import useFetch from '../hooks/fetch.hook';
 
 export default function Reset() {
-
-  const { username } = useAuthStore(state => state.auth)
+  const { username } = useAuthStore((state) => state.auth);
   const navigate = useNavigate();
-  const [{ isLoading, apiData, status, serverError }] = useFetch('createResetSession');
+  const [{ isLoading, status, serverError }] = useFetch('createResetSession');
 
   const formik = useFormik({
-    initialValues : {
-      password : 'admin@15',
-      confirm_pwd : 'admin'
+    initialValues: {
+      password: '',
+      confirm_pwd: '',
     },
-    validate : resetPasswordValidate,
-    validateOnBlur : false,
-    validateOnChange : false,
-    onSubmit : async values => {
+    validate: resetPasswordValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
       let resetPromise = resetPassword({ username, password: values.password });
 
       toast.promise(resetPromise, {
         loading: 'Updating...',
         success: <b>Reset Successfully...!</b>,
-        error : <b>Could not Reset!</b>
+        error: <b>Could not Reset!</b>,
       });
 
-      resetPromise.then(function(){ 
-        navigate('/password')
-      }).catch(error => { })
-    }
+      resetPromise
+        .then(function () {
+          navigate('/password');
+        })
+        .catch((error) => {});
+    },
   });
 
-  if (isLoading) return <h1>isLoading</h1>;
+  if (isLoading) return <h1>Loading...</h1>;
   if (serverError) return <h1 className="">{serverError}</h1>;
-  if (status && status !== 201) return <Navigate to={'/password'} replace={true} ></Navigate>;
+  if (status && status !== 201) return <Navigate to={'/password'} replace={true}></Navigate>;
 
   return (
+    <div
+      className="container-fluid d-flex align-items-center justify-content-center vh-100 position-relative"
+      style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/Background.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        position: 'relative',
+      }}
+    >
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
 
-    <div className="">
+      <div
+        className="position-absolute top-0 end-0 bottom-0 start-0"
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          backdropFilter: 'blur(5px)',
+        }}
+      ></div>
 
-      <Toaster position='' reverseOrder={false}></Toaster>
+      <div
+        className="rounded p-4"
+        style={{
+          width: '40%',
+          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          boxShadow: '0px 0px 15px 0px rgba(0, 0, 0, 0.1)',
+          position: 'relative',
+          zIndex: 1,
+        }}
+      >
+        <div className="text-center">
+          <h4 className="">Reset</h4>
+          <span>Enter new password.</span>
+        </div>
 
-      <div className=''>
-        <div className="" style={{width : "50%"}}>
-
-          <div className="">
-            <h4 className=''>Reset</h4>
-            <span className=''>
-              Enter new password.
-            </span>
+        <form onSubmit={formik.handleSubmit} className="mt-4">
+          <div className="mb-3">
+            <input
+              {...formik.getFieldProps('password')}
+              type="text"
+              className="form-control"
+              placeholder="New Password"
+            />
+            <input
+              {...formik.getFieldProps('confirm_pwd')}
+              type="password"
+              className="form-control mt-2"
+              placeholder="Repeat Password"
+            />
           </div>
 
-          <form className='' onSubmit={formik.handleSubmit}>
-              
-              <div className="">
-                  <input {...formik.getFieldProps('password')} type="passwordd" className="" placeholder='New Password' />
-                  <input {...formik.getFieldProps('confirm_pwd')} type="passwordd" className="" placeholder='Repeat Password' />
-                  <button type='submit' className=" ">Reset</button>
-              </div>
-
-          </form>
-
-        </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Reset
+          </button>
+        </form>
       </div>
-      <div className="background-section"></div>
     </div>
-  )
+  );
 }
